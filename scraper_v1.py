393688f41e4918487c9d28762227c084f3d31678
MarkTension <tensen.mark@gmail.com>
2019-06-06 14:57:54 +0200

    scraper works. Now only implement it for different websites and gotta make a flask app with DB

--- scraper_v1.py
+++ scraper_v1.py
@@ -9,6 +9,28 @@ from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup as bs
 
 
+#### make array with website xpaths
+"""
+1. make array with website htmls and xpaths
+- weeronline https://www.weeronline.nl/Europa/Nederland/Amsterdam/4058223
+- google https://www.google.com/search?q=weer+amsterdam&oq=weer+amsterdam&aqs=chrome..69i57j69i60l2j0l3.2016j0j4&sourceid=chrome&ie=UTF-8
+- weerplaza https://www.weerplaza.nl/nederland/amsterdam/5575/
+- weer.nl http://www.weer.nl/verwachting/nederland/amsterdam/1812872/
+------
+2. save in DB 
+ - SQLite:
+ plek       | tijd  |  site   |   temperatuur
+ ---------------------------------------------
+ amsterdam  |       |         |
+            |       |         |
+            |
+            |
+
+
+
+"""
+
+#
 
 
 
@@ -17,14 +39,15 @@ def main():
     driver = webdriver.Chrome('./chromedriver')
 
     driver.get('https://knmi.nl/nederland-nu/weer/verwachtingen')
-    driver.get('//*[@id="weather"]/div[1]/div/div[2]/div[2]/p[3]/a')
+    # driver.get('//*[@id="weather"]/div[1]/div/div[2]/div[2]/p[3]/a')
+
 
     # get access to full txt
     xpathname = '//*[@id="weather"]/div[1]/div/div[2]/div[2]/p[3]/a'
     xpath_element = driver.find_element(By.XPATH, xpathname)
     xpath_element.click()
-    xpath =
-    class_name =
+    # xpath =
+    # class_name =
 
     # get txt in string
     xpath_txt = '//*[@id="weather"]/div[1]/div/div[2]/div[2]'
